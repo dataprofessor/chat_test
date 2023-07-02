8ad5c988edd6c456db9665563b82e9a2ad23f1a8
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-02 10:26:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,24 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+# Works on Community Cloud
+# Works locally
+with st.chat_message("assistant", avatar='🤖' ):
+    st.write('Testing ... 1, 2, 3')
 
-st.write('Hello world!')
+# Works on Community Cloud
+# Works locally
+with st.chat_message("assistant", avatar='https://streamlit.io/images/brand/streamlit-mark-color.png' ):
+    st.write('Testing ... 1, 2, 3')
+
+# Not working on Community Cloud
+# Not working locally
+image = 'streamlit.png'
+with st.chat_message("assistant", avatar=image ):
+    st.write('Testing ... 1, 2, 3')
+
+# Not working on Community Cloud
+# Works locally
+from PIL import Image
+image = Image.open('streamlit.png')
+with st.chat_message("assistant", avatar=image ):
+    st.write('Testing ... 1, 2, 3')
